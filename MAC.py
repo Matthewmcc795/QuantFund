644fb7df6c562e70d02a2f755e2a872117a5fcd9
Matthew McCreath <matthewmccreath795@gmail.com>
2016-01-11 18:36:57 -0500

    Update

--- MAC.py
+++ MAC.py
@@ -12,29 +12,26 @@ Sec.append("EUR_USD")
 Sec.append("GBP_USD")
 Sec.append("AUD_USD")
 
-Bars = 50
+Bars = 51
 SL = 0.0006
 TP = 0.0004
 dt = datetime.strptime('January 11 16  12:00', '%B %d %y %H:%M')
 n = 50
 name = "Log file.txt"
-file = open(name,'a')
-file.close()
-file.write("hello world in the new file\n")
 
 while True:
 
     while True:
         if datetime.now() > dt:
             file = open(name,'a')
-            file.write("Running script\n")
+            file.write(str(datetime.now()) + " Running script\n")
             file.close()
             break 
         time.sleep(1)
 
     for i in range(0,3):
         file = open(name,'a')
-        file.write("Getting data for " + Sec[i] + "\n")
+        file.write(str(datetime.now()) + " Getting data for " + Sec[i] + "\n")
         file.close()
         h = {'Authorization' : ACCESS_TOKEN}
         url =   "https://api-fxpractice.oanda.com/v1/candles?instrument=" + str(Sec[i]) + "&count=" + str(Bars) + "&candleFormat=midpoint&granularity=H4&dailyAlignment=3"
@@ -68,7 +65,7 @@ while True:
         Upper_Band = SMA + 2*sd
         Lower_Band = SMA - 2*sd
         file = open(name,'a')
-        file.write("Checking open orders...\n")
+        file.write(str(datetime.now()) + " Checking open orders...\n")
         file.close()
         h = {'Authorization' : ACCESS_TOKEN}
         url = "https://api-fxpractice.oanda.com/v1/accounts/5801231/positions"
@@ -83,14 +80,14 @@ while True:
                 if positions["instrument"] == Sec[i]:
                     Open_Units = positions["units"]
         file = open(name,'a')
-        file.write("str(Open_Units) + " Units of " + str(Sec[i])\n")
+        file.write(str(datetime.now()) + " " + str(Open_Units) + " Units of " + str(Sec[i]) + "\n")
         file.close()
         if Close(0) > Upper_Band and Close(1) > Upper_Band and Open_Units == 0:
             file = open(name,'a')
-            file.write("Selling 200,000 of " + str(Sec[i]) + "\n")
-            file.write("Current Price is " + str(Close(0)) + "\n")
-            file.write("TP is " + str(round(Close(0) - TP - 0.0001,5)) + "\n")
-            file.write("SL is " + str(round(Close(0) + SL + 0.0001,5)) + "\n")
+            file.write(str(datetime.now()) + " Selling 200,000 of " + str(Sec[i]) + "\n")
+            file.write(str(datetime.now()) + " Current Price is " + str(Close(0)) + "\n")
+            file.write(str(datetime.now()) + " TP is " + str(round(Close(0) - TP - 0.0001,5)) + "\n")
+            file.write(str(datetime.now()) + " SL is " + str(round(Close(0) + SL + 0.0001,5)) + "\n")
             file.close() 
             conn = httplib.HTTPSConnection("api-fxpractice.oanda.com")
             headers = {"Content-Type": "application/x-www-form-urlencoded","Authorization": ACCESS_TOKEN}
@@ -104,15 +101,15 @@ while True:
             })
             conn.request("POST", "/v1/accounts/5801231/orders", params, headers)
             response = conn.getresponse().read()
-            file = Open(name,'a')
+            file = open(name,'a')
             file.write(response + "\n")
             file.close()
         elif Close(0) < Lower_Band and Close(1) < Lower_Band and Open_Units == 0:
             file = open(name,'a')
-            file.write("Buying 200,000 of " + str(Sec[i]) + "\n")
-            file.write("Current Price is " + str(Close(0)) + "\n")
-            file.write("TP is " + str(round(Close(0) + TP - 0.0001,5)) + "\n")
-            file.write("SL is " + str(round(Close(0) - SL + 0.0001,5)) + "\n")
+            file.write(str(datetime.now()) + " Buying 200,000 of " + str(Sec[i]) + "\n")
+            file.write(str(datetime.now()) + " Current Price is " + str(Close(0)) + "\n")
+            file.write(str(datetime.now()) + " TP is " + str(round(Close(0) + TP - 0.0001,5)) + "\n")
+            file.write(str(datetime.now()) + " SL is " + str(round(Close(0) - SL + 0.0001,5)) + "\n")
             file.close() 
             conn = httplib.HTTPSConnection("api-fxpractice.oanda.com")
             headers = {"Content-Type": "application/x-www-form-urlencoded","Authorization": ACCESS_TOKEN}
@@ -126,20 +123,20 @@ while True:
             })
             conn.request("POST", "/v1/accounts/5801231/orders", params, headers)
             response = conn.getresponse().read()
-            file = Open(name,'a')
-            file.write(str(response) + "\n")
+            file = open(name,'a')
+            file.write(str(datetime.now()) + " " + str(response) + "\n")
             file.close()
         elif Open_Units > 0:
-            file = Open(name,'a')
-            file.write("Positions still open for " + str(Sec[i]) + "\n")
+            file = open(name,'a')
+            file.write(str(datetime.now()) + " Positions still open for " + str(Sec[i]) + "\n")
             file.close()
         else:
-            file = Open(name,'a')
-            file.write("No trades available for " + str(Sec[i]) + "\n")
+            file = open(name,'a')
+            file.write(str(datetime.now()) + " No trades available for " + str(Sec[i]) + "\n")
             file.close()
 
     dt = datetime.now() + timedelta(hours=4)
     dt = dt.replace(minute=0,second=1,microsecond=0)
-    file = Open(name,'a')
-    file.write("Waiting until " + str(dt) + "\n")
+    file = open(name,'a')
+    file.write(str(datetime.now()) + " Waiting until " + str(dt) + "\n")
     file.close()
