8db05ebaad99c164f7aa292bfc9278bcd4f10e67
Matthew McCreath <matthewmccreath795@gmail.com>
2016-01-25 05:56:46 -0500

    Time Check2

--- Time_Check.py
+++ Time_Check.py
@@ -1,10 +1,19 @@
+import requests
+import json
+from Settings import PRICE_DOMAIN, ACCOUNT_DOMAIN, ACCESS_TOKEN, ACCOUNT_ID, STRT, STRO, STRH, STRL, STRC, STRV, STRCO
+import httplib
+import urllib
 from datetime import datetime, timedelta
 import time
-import sys
 
-dt = datetime.strptime('January 25 16 :30', '%B %d %y %H:%M')
-print dt
+h = {'Authorization' : ACCESS_TOKEN}
+url =   "https://api-fxpractice.oanda.com/v1/candles?instrument=EUR_USD&count=1&candleFormat=midpoint&granularity=S5"
+r = requests.get(url, headers=h)     
+data = json.loads(r.text)
+print data["candles"][0][STRT]
 print str(datetime.now())
+dt = datetime.strptime('January 25 16 11:30', '%B %d %y %H:%M')
+print dt
 dt = datetime.now() + timedelta(minutes=5)
 print dt
 dt = dt.replace(second=0,microsecond=1)
